646349e615fc13b912011319f8844f4a09742160
John Doe <mahdibenalaya566@gmail.com>
2025-02-17 13:43:50 +0100

    debug

--- demo/src/main/java/project/demo/controller/userController.java
+++ demo/src/main/java/project/demo/controller/userController.java
@@ -1,15 +1,24 @@
 package project.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import project.demo.model.user;
+import project.demo.repository.userRepository;
 
 
 
 @Controller
 public class userController {
 
+  private userRepository repository;
+  public userController(){
+    repository= new userRepository();
+  }
     @GetMapping("/user")
-      public String get(){
-        return "list";
+      public List<user> get(){
+        return repository.findAll();
       }
 }
